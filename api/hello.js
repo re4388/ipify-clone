a4105ad6b1645fb3fde554f4442f20ba246bff59
re4388 <re4388@gmail.com>
2020-05-31 16:09:00 +0800

    add hello

--- api/hello.js
+++ api/hello.js
@@ -2,5 +2,5 @@ const requestIp = require('request-ip')
 
 module.exports = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({ ip: requestIp.getClientIp(req) });
+    res.status(200).json({ hello: 'world' });
 }
