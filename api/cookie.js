93c41b0679c44b30b9fee8eec7da46d06f896406
re4388 <re4388@gmail.com>
2020-05-31 17:13:09 +0800

    change cookie API key and val

--- api/cookie.js
+++ api/cookie.js
@@ -1,7 +1,7 @@
 const { serialize } = require('cookie')
 
 module.exports = (req, res) => {
-    const cookie = serialize('ck', 'value', { httpOnly: true, expires: new Date('2020-12-01') })
+    const cookie = serialize('hello', 'world', { httpOnly: true, expires: new Date('2020-12-01') })
     res.setHeader('Set-Cookie', [cookie]);
     res.end('It worked, check your cookies in devtools');
 }
